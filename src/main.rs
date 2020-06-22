d18c5ffb04def004f2c3fe2a30bc6032bb6a9fa7
Hanno Braun <hanno@braun-embedded.com>
2020-06-22 13:44:19 +0200

    Light LED, if button is pressed

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,23 @@ use lpc8xx_hal::cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
+    let p = lpc8xx_hal::Peripherals::take().unwrap();
+
+    let mut syscon = p.SYSCON.split();
+    let     gpio   = p.GPIO.enable(&mut syscon.handle);
+
+    let button = p.pins.pio0_4.into_input_pin(gpio.tokens.pio0_4);
+    let mut led = p.pins.pio1_1.into_output_pin(
+        gpio.tokens.pio1_1,
+        lpc8xx_hal::gpio::Level::High,
+    );
+
     loop {
-        lpc8xx_hal::cortex_m::asm::nop();
+        if button.is_high() {
+            led.set_high();
+        }
+        else {
+            led.set_low();
+        }
     }
 }
